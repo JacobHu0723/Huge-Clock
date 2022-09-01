7b0a94594ae3586442b720d6f50156d7ee1cc654
JacobHu0723 <63916353+JacobHu0723@users.noreply.github.com>
2022-09-01 15:02:20 +0800

    PWA V1

---
+++ sw.js
@@ -0,0 +1,39 @@
+﻿var cacheStorageKey = 'minimal-pwa-1'
+
+var cacheList = [
+  '/',
+  "index.html",
+  "clock.png"
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  )
+})
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    Promise.all(
+      caches.keys().then(cacheNames => {
+        return cacheNames.map(name => {
+          if (name !== cacheStorageKey) {
+            return caches.delete(name)
+          }
+        })
+      })
+    ).then(() => {
+      return self.clients.claim()
+    })
+  )
+})
